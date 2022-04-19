96b5552927bddd743e144741172773791889d937
Abdulmalik <65926554+abdulmalikadeyemo@users.noreply.github.com>
2022-04-19 15:36:48 +0100

    Update app.py

--- app.py
+++ app.py
@@ -15,33 +15,8 @@ from io import BytesIO
 import pathlib
 plt = platform.system()
 if plt == 'Linux': pathlib.WindowsPath = pathlib.PosixPath
-    
-import base64
 
 path = Path('.')
-
-@st.cache(allow_output_mutation=True)
-def get_base64_of_bin_file(bin_file):
-    with open(bin_file, 'rb') as f:
-        data = f.read()
-    return base64.b64encode(data).decode()
-
-def set_png_as_page_bg(png_file):
-    bin_str = get_base64_of_bin_file(png_file)
-    page_bg_img = '''
-    <style>
-    body {
-    background-image: url("data:image/png;base64,%s");
-    background-size: cover;
-    }
-    </style>
-    ''' % bin_str
-    
-    st.markdown(page_bg_img, unsafe_allow_html=True)
-    return
-
-set_png_as_page_bg(path/'dr_background.png')
-
 
 
 def get_x(r): return image_path/r['train_image_name']
